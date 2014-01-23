04954f622174e7f0d36e69662215ae1e0fc4db55
Chapman Seto <chapman.seto@gmail.com>
2014-01-23 18:25:39 +0000

    javascript lab

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -11,8 +11,40 @@ $(document).ready(function() {
 function initializePage() {
 	$("#testjs").click(function(e) {
 		$('.jumbotron h1').text("Javascript is connected");
+		$('#testjs').text("The answer to your questions?");
+		$(".jumbotron p").toggleClass("active");
+	});
+
+	$("#submitBtn").click(function(e) {
+		var proj = $("#project").val();
+		$(proj).width($("#width").val());
+		var description = $(proj).find(".project-description");
+    	if (description.length != 0) { 
+        	description.html("<p>" + $("#description").val() + "</p>");
+    	}
 	});
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+	$("a.thumbnail").click(projectClick);
+}
+
+function projectClick(e) {
+  // Cancel the default action, which prevents the page from reloading
+    e.preventDefault();
+
+    // In an event listener, $(this) is the leement that fired the event
+    var projectTitle = $(this).find("p").text();
+    var jumbotronHeader = $(".jumbotron h1");
+    jumbotronHeader.text(projectTitle);
+	
+	var containingProject = $(this).closest(".project"); 
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) {
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+    } else if (description.is(":visible")){
+       description.hide();
+    } else {
+    	description.show();
+    }
 }
